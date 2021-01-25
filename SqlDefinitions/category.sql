aa5b0e87f5020d3a49eb98208ddf3227cb7eea64
George Kleanthous <george.kleanthous@hotmail.com>
2021-01-26 01:29:22 +0200

    sql definitions

---
+++ SqlDefinitions/category.sql
@@ -0,0 +1,21 @@
+USE [cynewscorner]
+GO
+
+/****** Object:  Table [dbo].[category]    Script Date: 1/26/2021 1:28:29 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[category](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](45) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
